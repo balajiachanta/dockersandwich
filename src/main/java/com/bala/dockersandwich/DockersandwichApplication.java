1fe8cc381a4d1ab90d1f31b4c75fedf56f83ebcc
BalajiAchanta <balajiachanta@gmail.com>
2021-01-20 11:55:55 -0600

    Initial commit

---
+++ src/main/java/com/bala/dockersandwich/DockersandwichApplication.java
@@ -0,0 +1,20 @@
+package com.bala.dockersandwich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DockersandwichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockersandwichApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String readHello(){
+		return "hello balu...";
+	}
+}
---
+++ src/test/java/com/bala/dockersandwich/DockersandwichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.dockersandwich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockersandwichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
